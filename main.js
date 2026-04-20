2e7fc8ed2f2d6dfdeae0f74715fe414c927a79b5
juanma <juanmanp13@gmail.com>
2026-04-20 00:21:07 -0300

    arreglos celular

--- main.js
+++ main.js
@@ -53,3 +53,34 @@ dots.forEach((dot, index) => {
         updateCarousel();
     });
 });
+
+let startX = 0;
+let endX = 0;
+
+const trackContainer = document.querySelector(".carousel-track-container");
+
+if (trackContainer) {
+    trackContainer.addEventListener("touchstart", (e) => {
+        startX = e.touches[0].clientX;
+    });
+
+    trackContainer.addEventListener("touchend", (e) => {
+        endX = e.changedTouches[0].clientX;
+        handleSwipe();
+    });
+}
+
+function handleSwipe() {
+    const diff = startX - endX;
+    const minSwipeDistance = 50;
+
+    if (Math.abs(diff) < minSwipeDistance) return;
+
+    if (diff > 0) {
+        currentSlide = (currentSlide + 1) % slides.length;
+    } else {
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    }
+
+    updateCarousel();
+}
